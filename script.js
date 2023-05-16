1c10ae8a0f5a9734097a05f33510fefa77925711
deboramds <deborah.mart@hotmail.com>
2023-05-16 16:55:46 -0300

    exercicio condicional

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 // var idade = prompt('digite sua idade');
 
+//função entrar
+
+function entrar() {
+
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome');
+
+    if(texto == '' || texto == null){
+        alert('digite seu nome novamente!');
+        area.innerHTML = 'Bem vindo ...';
+    }else{
+        area.innerHTML = 'Bem vindo ' + texto;
+    }
+}
+
+
+function entrar2(nome){
+ var area = document.getElementById('area2');
+ var texto = prompt('digite seu sobrenome');
+ area.innerHTML = nome + " " + texto;
+
+}
+   
